0726a1ced6089fa13879bafd132171cb99f8ed2b
KYs <amitkys59@gmail.com>
2024-10-11 01:36:33 +0530

    random testing

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     })
 });
 
+app.get('/kys/:id', (req, res) => {
+    const {id} = req.params;
+    // console.log(id);
+    res.json({message: `you have inserted ${id}`});
+})
 
 app.listen(3000, () => {
     console.log('app is listening on 3000');
